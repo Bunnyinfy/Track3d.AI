5db191f31be641e058717e81ed0728a90205f1a1
Bunnyinfy <penugondasrinivas20@gmail.com>
2025-05-19 12:01:34 +0530

    Added the logo

--- app.py
+++ app.py
@@ -625,7 +625,7 @@ def main():
 
     # Show app header
     st.markdown(
-        '<div class="main-header">🏗️ Construction Material Recommendation System</div>',
+        '<div class="main-header">🧱 Construction Material Recommendation System</div>',
         unsafe_allow_html=True,
     )
     st.markdown(
